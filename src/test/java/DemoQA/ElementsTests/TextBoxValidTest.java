0b94c3725e3e05b3993ce1a0e303048b36c5df45
Artur Demichev <artur.demichev@gmail.com>
2025-09-23 19:30:56 +0200

    test(demoqa): add URL text-box test and input test

---
+++ src/test/java/DemoQA/ElementsTests/TextBoxValidTest.java
@@ -0,0 +1,93 @@
+package DemoQA.ElementsTests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+
+public class TextBoxValidTest {
+
+    WebDriver driver;
+    WebDriverWait wait;
+    String mainURL = "https://demoqa.com";
+
+    @BeforeTest
+    public void prep() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        driver.get(mainURL);
+        WebElement elements = driver.findElement(By.xpath("//*[text()='Elements']"));
+        elements.click();
+    }
+
+    @Test
+    public void textBoxTest() {
+        wait.until(ExpectedConditions.urlContains("/elements"));
+
+        String currentURL = driver.getCurrentUrl();
+        Assert.assertNotNull(currentURL);
+        Assert.assertTrue(currentURL.contains("/elements"));
+
+        WebElement textBoxElement = driver.findElement(By.id("item-0"));
+        textBoxElement.click();
+
+        wait.until(ExpectedConditions.urlContains("/text-box"));
+        String textBoxURL = driver.getCurrentUrl();
+        Assert.assertTrue(textBoxURL.contains("/text-box"));
+
+    }
+
+    @Test
+    public void textBoxUserFormTest() {
+        WebElement nameInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("userName")));
+        WebElement emailInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("userEmail")));
+        WebElement currentAddressInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("currentAddress")));
+        WebElement permanentAddressInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("permanentAddress")));
+        WebElement submitButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("submit")));
+
+        Assert.assertTrue(nameInput.isEnabled());
+        Assert.assertTrue(emailInput.isEnabled());
+        Assert.assertTrue(currentAddressInput.isEnabled());
+        Assert.assertTrue(permanentAddressInput.isEnabled());
+        Assert.assertTrue(submitButton.isEnabled());
+
+        // Input data
+        String nameTest = "Arthur Demichev";
+        String emailTest = "arthur.demichev@yahoo.net";
+        String addressTest = "Cracow, Poland";
+
+        nameInput.sendKeys(nameTest);
+        emailInput.sendKeys(emailTest);
+        currentAddressInput.sendKeys(addressTest);
+        permanentAddressInput.sendKeys(addressTest);
+        submitButton.click();
+
+        WebElement outputElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("output")));
+        String outputText = outputElement.getText();
+
+        Assert.assertTrue(outputText.contains(nameTest));
+        Assert.assertTrue(outputText.contains(emailTest));
+        Assert.assertTrue(outputText.contains(addressTest));
+        Assert.assertTrue(outputText.contains(addressTest));
+    }
+
+
+    @AfterTest
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
