5b41d19e6b0127f082c2dea2dc14afc07f99916b
Artur Demichev <artur.demichev@gmail.com>
2025-09-26 22:08:52 +0200

    test(demoqa): add "ListInspector" withassertAllCheckboxesEnabled and assertAllResultsText methods to Helpers and add code in checkBox Tests and refactoring elements

--- src/test/java/DemoQA/ElementsTests/ElementsValidTests.java
+++ src/test/java/DemoQA/ElementsTests/ElementsValidTests.java
@@ -1,5 +1,7 @@
 package DemoQA.ElementsTests;
 
+import DemoQA.Helpers.ListInspector;
+import DemoQA.Pages.CheckboxPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -17,9 +19,11 @@ import java.util.List;
 
 public class ElementsValidTests {
 
-    WebDriver driver;
     WebDriverWait wait;
     String mainURL = "https://demoqa.com";
+    CheckboxPage checkboxPage;
+    ListInspector listInspector = new ListInspector();
+    private WebDriver driver;
 
     @BeforeTest
     public void prep() {
@@ -93,116 +97,63 @@ public class ElementsValidTests {
         String checkBoxURL = driver.getCurrentUrl();
         Assert.assertNotNull(checkBoxURL);
         Assert.assertTrue(checkBoxURL.contains("/checkbox"));
+        checkboxPage = new CheckboxPage();
+        listInspector.setDriver(driver);
     }
 
     @Test(priority = 2, dependsOnMethods = {"checkBoxTest"})
     public void checkBox_Expand_Collapse_AllBtnTest() {
-        WebElement expandAllBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".rct-option-expand-all")));
-        WebElement collapseAllBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".rct-option-collapse-all")));
-        Assert.assertTrue(expandAllBtn.isDisplayed());
+        WebElement expandAllBtn = driver.findElement(checkboxPage.expandAllBtnLocator);
+        WebElement collapseAllBtn = driver.findElement(checkboxPage.collapseAllBtnLocator);
         Assert.assertTrue(expandAllBtn.isEnabled());
-        Assert.assertTrue(collapseAllBtn.isDisplayed());
         Assert.assertTrue(collapseAllBtn.isEnabled());
 
         // Default state - all collapsed
-        List<WebElement> defaultItems = driver.findElements(By.className("rct-node-collapsed"));
-        int defaultItemsCount = defaultItems.size();
-        Assert.assertEquals(defaultItemsCount, 1);
+        List<WebElement> collapsedItems = driver.findElements(checkboxPage.collapsedTreeLocator);
+        int collapsedItemsCount = collapsedItems.size();
+        Assert.assertEquals(collapsedItemsCount, 1);
         // Click Expand All
         expandAllBtn.click();
-        //After click - all expanded
-        List<WebElement> expandedItems = driver.findElements(By.className("rct-node-expanded"));
+        //After click - find all expanded
+        List<WebElement> expandedItems = driver.findElements(checkboxPage.expandedTreeLocator);
         int expandedItemsCount = expandedItems.size();
+        // After click - find no collapsed
+        collapsedItems = driver.findElements(checkboxPage.collapsedTreeLocator);
+        collapsedItemsCount = collapsedItems.size();
+        // Verify counts
         Assert.assertEquals(expandedItemsCount, 6);
+        Assert.assertEquals(collapsedItemsCount, 0);
 
         // Check that there all tree-nodes are displayed and enabled
 
-        WebElement home_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-home'] span.rct-checkbox")));
-        WebElement desktop_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-desktop'] span.rct-checkbox")));
-        WebElement notes_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-notes'] span.rct-checkbox")));
-        WebElement commands_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-commands'] span.rct-checkbox")));
-        WebElement documents_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-documents'] span.rct-checkbox")));
-        WebElement workspace_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-workspace'] span.rct-checkbox")));
-        WebElement react_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-react'] span.rct-checkbox")));
-        WebElement angular_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-angular'] span.rct-checkbox")));
-        WebElement veu_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-veu'] span.rct-checkbox")));
-        WebElement office_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-office'] span.rct-checkbox")));
-        WebElement public_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-public'] span.rct-checkbox")));
-        WebElement private_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-private'] span.rct-checkbox")));
-        WebElement classified_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-classified'] span.rct-checkbox")));
-        WebElement general_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-general'] span.rct-checkbox")));
-        WebElement downloads_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-downloads'] span.rct-checkbox")));
-        WebElement wordFile_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-wordFile'] span.rct-checkbox")));
-        WebElement excelFile_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-excelFile'] span.rct-checkbox")));
-        Assert.assertTrue(home_Checkbox.isDisplayed());
-        Assert.assertTrue(home_Checkbox.isEnabled());
-        Assert.assertTrue(desktop_Checkbox.isDisplayed());
-        Assert.assertTrue(desktop_Checkbox.isEnabled());
-        Assert.assertTrue(notes_Checkbox.isDisplayed());
-        Assert.assertTrue(notes_Checkbox.isEnabled());
-        Assert.assertTrue(commands_Checkbox.isDisplayed());
-        Assert.assertTrue(commands_Checkbox.isEnabled());
-        Assert.assertTrue(documents_Checkbox.isDisplayed());
-        Assert.assertTrue(documents_Checkbox.isEnabled());
-        Assert.assertTrue(workspace_Checkbox.isDisplayed());
-        Assert.assertTrue(workspace_Checkbox.isEnabled());
-        Assert.assertTrue(react_Checkbox.isDisplayed());
-        Assert.assertTrue(react_Checkbox.isEnabled());
-        Assert.assertTrue(angular_Checkbox.isDisplayed());
-        Assert.assertTrue(angular_Checkbox.isEnabled());
-        Assert.assertTrue(veu_Checkbox.isDisplayed());
-        Assert.assertTrue(veu_Checkbox.isEnabled());
-        Assert.assertTrue(office_Checkbox.isDisplayed());
-        Assert.assertTrue(office_Checkbox.isEnabled());
-        Assert.assertTrue(public_Checkbox.isDisplayed());
-        Assert.assertTrue(public_Checkbox.isEnabled());
-        Assert.assertTrue(private_Checkbox.isDisplayed());
-        Assert.assertTrue(private_Checkbox.isEnabled());
-        Assert.assertTrue(classified_Checkbox.isDisplayed());
-        Assert.assertTrue(classified_Checkbox.isEnabled());
-        Assert.assertTrue(general_Checkbox.isDisplayed());
-        Assert.assertTrue(general_Checkbox.isEnabled());
-        Assert.assertTrue(downloads_Checkbox.isDisplayed());
-        Assert.assertTrue(downloads_Checkbox.isEnabled());
-        Assert.assertTrue(wordFile_Checkbox.isDisplayed());
-        Assert.assertTrue(wordFile_Checkbox.isEnabled());
-        Assert.assertTrue(excelFile_Checkbox.isDisplayed());
-        Assert.assertTrue(excelFile_Checkbox.isEnabled());
+        listInspector.assertAllCheckboxesEnabled(checkboxPage.checkBoxListLocators);
+
+
         // Click Collapse All
         collapseAllBtn.click();
-        // After click - all collapsed and return to default state
-        List<WebElement> collapsedItems = driver.findElements(By.className("rct-node-collapsed"));
-        int collapsedItemsCount = collapsedItems.size();
+        // After click - all collapsed and return to default state, find all collapsed and check again
+        collapsedItems = driver.findElements(checkboxPage.collapsedTreeLocator);
+        collapsedItemsCount = collapsedItems.size();
         Assert.assertEquals(collapsedItemsCount, 1);
     }
 
     @Test(priority = 2, dependsOnMethods = {"checkBoxTest"})
     public void checkBox_Select_Home_CheckboxTest() {
-        WebElement home_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-home'] span.rct-checkbox")));
-        Assert.assertTrue(home_Checkbox.isDisplayed());
+        WebElement home_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(checkboxPage.home_Checkbox));
         Assert.assertTrue(home_Checkbox.isEnabled());
         home_Checkbox.click();
 
-        WebElement resultElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("result")));
-        String resultText = resultElement.getText();
-        Assert.assertTrue(resultText.contains("You have selected :"));
-        Assert.assertTrue(resultText.contains("home"));
-        Assert.assertTrue(resultText.contains("desktop"));
-        Assert.assertTrue(resultText.contains("notes"));
-        Assert.assertTrue(resultText.contains("commands"));
-        Assert.assertTrue(resultText.contains("documents"));
-        Assert.assertTrue(resultText.contains("workspace"));
-        Assert.assertTrue(resultText.contains("react"));
-        Assert.assertTrue(resultText.contains("angular"));
-        Assert.assertTrue(resultText.contains("veu"));
-        Assert.assertTrue(resultText.contains("office"));
-        Assert.assertTrue(resultText.contains("public"));
-        Assert.assertTrue(resultText.contains("private"));
-        Assert.assertTrue(resultText.contains("classified"));
-        Assert.assertTrue(resultText.contains("general"));
-        Assert.assertTrue(resultText.contains("downloads"));
-        Assert.assertTrue(resultText.contains("wordFile"));
-        Assert.assertTrue(resultText.contains("excelFile"));
+        wait.until(ExpectedConditions.numberOfElementsToBe(
+                checkboxPage.resultItemsLocator,
+                checkboxPage.expectedResults.size()
+        ));
+
+        List<String> actualTexts = driver.findElements(checkboxPage.resultItemsLocator)
+                .stream()
+                .map(WebElement::getText) // get innerText from <span class="text-success">
+                .toList();
+
+        listInspector.assertAllResultsText(checkboxPage.expectedResults, actualTexts);
     }
 
     @AfterTest
---
+++ src/test/java/DemoQA/Pages/CheckboxPage.java
@@ -0,0 +1,64 @@
+package DemoQA.Pages;
+
+import org.openqa.selenium.By;
+
+import java.util.List;
+
+public class CheckboxPage {
+
+    public By expandAllBtnLocator = By.cssSelector(".rct-option-expand-all");
+    public By collapseAllBtnLocator = By.cssSelector(".rct-option-collapse-all");
+
+    public By collapsedTreeLocator = By.cssSelector(".rct-node.rct-node-collapsed");
+    public By expandedTreeLocator = By.cssSelector(".rct-node.rct-node-expanded");
+
+    public By resultItemsLocator = By.cssSelector("#result .text-success");
+
+    public List<By> checkBoxListLocators = List.of(
+            By.cssSelector("label[for='tree-node-home'] span.rct-checkbox"),
+            By.cssSelector("label[for='tree-node-desktop'] span.rct-checkbox"),
+            By.cssSelector("label[for='tree-node-notes'] span.rct-checkbox"),
+            By.cssSelector("label[for='tree-node-commands'] span.rct-checkbox"),
+            By.cssSelector("label[for='tree-node-documents'] span.rct-checkbox"),
+            By.cssSelector("label[for='tree-node-workspace'] span.rct-checkbox"),
+            By.cssSelector("label[for='tree-node-react'] span.rct-checkbox"),
+            By.cssSelector("label[for='tree-node-angular'] span.rct-checkbox"),
+            By.cssSelector("label[for='tree-node-veu'] span.rct-checkbox"),
+            By.cssSelector("label[for='tree-node-office'] span.rct-checkbox"),
+            By.cssSelector("label[for='tree-node-public'] span.rct-checkbox"),
+            By.cssSelector("label[for='tree-node-private'] span.rct-checkbox"),
+            By.cssSelector("label[for='tree-node-classified'] span.rct-checkbox"),
+            By.cssSelector("label[for='tree-node-general'] span.rct-checkbox"),
+            By.cssSelector("label[for='tree-node-downloads'] span.rct-checkbox"),
+            By.cssSelector("label[for='tree-node-wordFile'] span.rct-checkbox"),
+            By.cssSelector("label[for='tree-node-excelFile'] span.rct-checkbox")
+    );
+
+    public List<String> expectedResults = List.of(
+            "home",
+            "desktop", "notes", "commands",
+            "documents", "workspace", "react", "angular", "veu",
+            "office", "public", "private", "classified", "general",
+            "downloads", "wordFile", "excelFile"
+    );
+
+
+    // Individual locators if needed
+    public By home_Checkbox = By.cssSelector("label[for='tree-node-home'] span.rct-checkbox");
+//        WebElement desktop_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-desktop'] span.rct-checkbox")));
+//        WebElement notes_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-notes'] span.rct-checkbox")));
+//        WebElement commands_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-commands'] span.rct-checkbox")));
+//        WebElement documents_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-documents'] span.rct-checkbox")));
+//        WebElement workspace_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-workspace'] span.rct-checkbox")));
+//        WebElement react_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-react'] span.rct-checkbox")));
+//        WebElement angular_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-angular'] span.rct-checkbox")));
+//        WebElement veu_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-veu'] span.rct-checkbox")));
+//        WebElement office_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-office'] span.rct-checkbox")));
+//        WebElement public_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-public'] span.rct-checkbox")));
+//        WebElement private_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-private'] span.rct-checkbox")));
+//        WebElement classified_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-classified'] span.rct-checkbox")));
+//        WebElement general_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-general'] span.rct-checkbox")));
+//        WebElement downloads_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-downloads'] span.rct-checkbox")));
+//        WebElement wordFile_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-wordFile'] span.rct-checkbox")));
+//        WebElement excelFile_Checkbox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("label[for='tree-node-excelFile'] span.rct-checkbox")));
+}
