ba97a1e58f0258cdea6f5c5c594895892d4383fc
Artur Demichev <artur.demichev@gmail.com>
2025-10-09 16:45:45 +0200

    test(demoqa): Create "AlertsPage" and "AlertsTests" classes. Found all elements from AlertsPage. Tested all alerts on the page.

---
+++ src/test/java/DemoQA/ElementsTests/AlertsTests.java
@@ -0,0 +1,57 @@
+package DemoQA.ElementsTests;
+
+
+import DemoQA.Helpers.ScreenshotOnFailureListener;
+import DemoQA.data.Pages.AlertsPage;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners(ScreenshotOnFailureListener.class)
+public class AlertsTests extends BaseTest {
+    AlertsPage alertsPage = new AlertsPage();
+
+    @BeforeMethod
+    public void prep() {
+        open("alerts");
+    }
+
+    @Test
+    public void testAlertButton() {
+        WebElement alertBtn = getDriver().findElement(alertsPage.alertButton);
+        alertBtn.click();
+        getDriver().switchTo().alert().accept();
+    }
+
+    @Test
+    public void testTimerAlertButton() throws InterruptedException {
+        WebElement timerAlertBtn = getDriver().findElement(alertsPage.timerAlertButton);
+        timerAlertBtn.click();
+        Thread.sleep(6000);
+        getDriver().switchTo().alert().accept();
+    }
+
+    @Test
+    public void testConfirmButton() {
+        WebElement confirmBtn = getDriver().findElement(alertsPage.confirmButton);
+        confirmBtn.click();
+        getDriver().switchTo().alert().dismiss();
+        WebElement confirmResult = getDriver().findElement(alertsPage.confirmResult);
+        Assert.assertEquals(confirmResult.getText(), "You selected Cancel");
+        confirmBtn.click();
+        getDriver().switchTo().alert().accept();
+        Assert.assertEquals(confirmResult.getText(), "You selected Ok");
+    }
+
+    @Test
+    public void testPromptButton() {
+        WebElement promptBtn = getDriver().findElement(alertsPage.promptButton);
+        promptBtn.click();
+        getDriver().switchTo().alert().sendKeys("Test");
+        getDriver().switchTo().alert().accept();
+        WebElement promptResult = getDriver().findElement(alertsPage.promptResult);
+        Assert.assertEquals(promptResult.getText(), "You entered Test");
+    }
+}
