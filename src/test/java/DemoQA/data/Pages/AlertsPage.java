42bd752c4d5b17548b4cb1a1257d22ee39037a5f
Artur Demichev <artur.demichev@gmail.com>
2025-10-09 17:16:26 +0200

    test(demoqa): Fill AlertsPage with new elements references, made several tests to check all alerts

--- src/test/java/DemoQA/data/Pages/AlertsPage.java
+++ src/test/java/DemoQA/data/Pages/AlertsPage.java
@@ -1,4 +1,15 @@
 package DemoQA.data.Pages;
 
+import org.openqa.selenium.By;
+
 public class AlertsPage {
+    public By alertButton = By.id("alertButton");
+    public By timerAlertButton = By.id("timerAlertButton");
+
+    public By confirmButton = By.id("confirmButton");
+    public By promptButton = By.id("promtButton");
+
+    public By confirmResult = By.id("confirmResult");
+    public By promptResult = By.id("promptResult");
+
 }
