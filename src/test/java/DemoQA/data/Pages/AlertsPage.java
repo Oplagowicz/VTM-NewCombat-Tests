84e61532e59be2508c607e1d8af9453a82d2b274
Artur Demichev <artur.demichev@gmail.com>
2025-10-09 17:34:00 +0200

    test(demoqa): Remove wide spaces

--- src/test/java/DemoQA/data/Pages/AlertsPage.java
+++ src/test/java/DemoQA/data/Pages/AlertsPage.java
@@ -5,10 +5,8 @@ import org.openqa.selenium.By;
 public class AlertsPage {
     public By alertButton = By.id("alertButton");
     public By timerAlertButton = By.id("timerAlertButton");
-
     public By confirmButton = By.id("confirmButton");
     public By promptButton = By.id("promtButton");
-
     public By confirmResult = By.id("confirmResult");
     public By promptResult = By.id("promptResult");
 
