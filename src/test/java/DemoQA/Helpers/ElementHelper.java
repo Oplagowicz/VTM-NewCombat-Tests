f1447f1b9f55998e1cbcc3d4cd6f937b417a1d88
Artur Demichev <artur.demichev@gmail.com>
2025-10-22 00:05:10 +0200

    test(demoqa): Added Log4J dependency and implemented LoggerFactory into "ElementHelper.java" for more accurate logging

--- src/test/java/DemoQA/Helpers/ElementHelper.java
+++ src/test/java/DemoQA/Helpers/ElementHelper.java
@@ -3,37 +3,49 @@ package DemoQA.Helpers;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ElementHelper {
     WebDriver driver;
+    private static final Logger logger = LoggerFactory.getLogger(ElementHelper.class);
 
     public ElementHelper(WebDriver driver) {
         this.driver = driver;
     }
 
+
+    /**
+     * Returns true if the element becomes visible within the wait timeout.
+     */
+
     public Boolean elementExists(By locator) {
         try {
-            System.out.println("Element is exist: " + locator.toString());
+            logger.debug("Checking existence (visibility) of element: {}", locator);
             // Wait until the element is visible
             WebElement button = driver.findElement(locator);
             // Perform actions on the element
-            System.out.println("end");
+            logger.info("end");
             return button.isDisplayed();
         } catch (Exception e) {
-            System.out.println("exeption");
+            logger.error("Unexpected error while checking element existence: {}", locator, e);
             return false;
         }
     }
 
+    /**
+     * Returns true when the element is not present or not visible (i.e. disappeared).
+     */
+
     public Boolean elementIsDisappear(By locator) {
         try {
-            System.out.println("Element is disappear: " + locator.toString());
+            logger.debug("Waiting for element to disappear: {}", locator);
             WebElement button = driver.findElement(locator);
             // Perform actions on the element
-            System.out.println("end");
+            logger.info("end");
             return button.isDisplayed();
         } catch (Exception e) {
-            System.out.println("exeption");
+            logger.error("exception");
             return true;
         }
     }
